9b0b584205cbf7c4c34312da27eed5232f9defbb
Cooper Weinhold <cjweinhold@mail.fhsu.edu>
2024-10-10 13:59:36 -0500

    Created auth templates and style css.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -9,7 +9,6 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -37,57 +36,34 @@ def register():
 
         flash(error)
 
-    @bp.route('/login', methods=('GET', 'POST'))
-    def login():
-        if request.method == 'POST':
-            username = request.form['username']
-            password = request.form['password']
-            db = get_db()
-            error = None
-            user = db.execute(
-                'SELECT * FROM user WHERE username = ?', (username,)
-            ).fetchone()
-
-            if user is None:
-                error = 'Incorrect username.'
-            elif not check_password_hash(user['password'], password):
-                error = 'Incorrect password.'
-
-            if error is None:
-                session.clear()
-                session['user_id'] = user['id']
-                return redirect(url_for('index'))
-
-            flash(error)
-
-        return render_template('auth/login.html')
-
-    @bp.route('/login', methods=('GET', 'POST'))
-    def login():
-        if request.method == 'POST':
-            username = request.form['username']
-            password = request.form['password']
-            db = get_db()
-            error = None
-            user = db.execute(
-                'SELECT * FROM user WHERE username = ?', (username,)
-            ).fetchone()
-
-            if user is None:
-                error = 'Incorrect username.'
-            elif not check_password_hash(user['password'], password):
-                error = 'Incorrect password.'
-
-            if error is None:
-                session.clear()
-                session['user_id'] = user['id']
-                return redirect(url_for('index'))
-
-            flash(error)
-
-        return render_template('auth/login.html')
-
     return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+
 
 @bp.before_app_request
 def load_logged_in_user():
